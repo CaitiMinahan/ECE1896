9eac6bc252093fdcf4d9ad8316088b3e43afd3bb
ldt9 <84938803+ldt9@users.noreply.github.com>
2024-02-06 20:48:12 -0500

    added files

--- utils/python/locally_weighted_regression.py
+++ utils/python/locally_weighted_regression.py
@@ -2,6 +2,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# TODO: look up how to reference the csv given bus values for V and I as well as SOH from model for predicting SOC
+# TODO: How to round array values to 3 decimal places
+# TODO: What is the best K value to use for the locally weighted regression
+# TODO: How to compress the output for microcontroller use
+
 # Gaussian kernel
 def kernel(point, xmat, k):
     m,n = np.shape(xmat)
@@ -29,25 +34,50 @@ def localWeightRegression(xmat, ymat, k):
 
 data = pd.read_csv('../../res/model_data/batemo_model_data.csv')
 data = data[data.V >= 2.9]
-colA = np.array(data.V)
-colB = np.array(data.SOC)
 
-mcolA = np.mat(colA)
-mcolB = np.mat(colB)
+min_SOH = data.SOH.min()
+max_SOH = data.SOH.max()
+min_I = data.I.min()
+max_I = data.I.max()
 
-m = np.shape(mcolB)[1]
-one = np.ones((1, m), dtype = int)
+# for i in range(min_SOH, max_SOH + 1):
+#     for j in range(min_I, max_I + 1):
 
-# horizontal stacking
-X = np.hstack((one.T, mcolA.T))
-print(X.shape)
+for i in range(90,91):
+    for j in range(10,11):
 
-ypred = localWeightRegression(X, mcolB, 0.5)
+        data = data[data.SOH == i]
+        data = data[data.I == j]
 
-xsort = X.copy()
-xsort.sort(axis=0)
-plt.scatter(colA, colB, color='blue')
-plt.plot(xsort[:, 1], ypred[X[:, 1].argsort(0)], color='yellow', linewidth=5)
-plt.xlabel('V')
-plt.ylabel('SOC')
-plt.show()
+        colA = np.array(data.V)
+        colB = np.array(data.SOC)
+
+        mcolA = np.mat(colA)
+        mcolB = np.mat(colB)
+
+        m = np.shape(mcolB)[1]
+        one = np.ones((1, m), dtype = int)
+
+        # horizontal stacking
+        X = np.hstack((one.T, mcolA.T))
+        print(X.shape)
+
+        ypred = localWeightRegression(X, mcolB, 0.1)
+
+        # append the data to a csv
+        export = pd.DataFrame()
+        export['V'] = round(colA,3)
+        export['I'] = j
+        export['SOH'] = i
+        export['SOC'] = round(ypred,3)
+        export.to_csv('new csv.csv', mode='a', header=False)
+
+        # xsort = X.copy()
+        # xsort.sort(axis=0)
+        # plt.scatter(colA, colB, color='blue')
+        # #                V           SOC
+        # plt.plot(xsort[:, 1], ypred[X[:, 1].argsort(0)], color='yellow', linewidth=5)
+        # plt.xlabel('V')
+        # plt.ylabel('SOC')
+        # plt.show()
+
